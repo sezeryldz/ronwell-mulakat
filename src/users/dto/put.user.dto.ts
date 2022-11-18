4d8428c4eca56067f4005c629b5d7d99136d2dba
Sezer Y <sezer339@gmail.com>
2022-11-18 14:46:23 +0300

    feature: rest api w/o custom features

---
+++ src/users/dto/put.user.dto.ts
@@ -0,0 +1,10 @@
+export interface PutUserDto {
+  id: string
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  type: string
+  UpdatedAt: number
+  CreatedAt: number
+}
